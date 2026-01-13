e117e85637a605ec737e5a967338be9fa06f4861
shankar.aravind <ashankar1995@gmail.com>
2026-01-13 11:42:48 -0500

    updated cart item logic

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,45 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let totalAmount = 0;
+    cart.forEach((item) => {
+        totalAmount = totalAmount + (item.quantity * parseFloat(item.cost.substring(1)));
+    });
+    return totalAmount; 
   };
 
   const handleContinueShopping = (e) => {
-   
+    onContinueShopping(e);
   };
 
 
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
+
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
    
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+
+    let totalCost = 0;
+    cart.forEach((item) => {
+        totalCost = totalCost + (item.quantity * parseFloat(item.cost.substring(1)));
+    });
+    return totalCost; 
+  };
+
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
   };
 
   return (
